f2bc36661484abb8c7eb6a79a4955dfbdc29ea3c
ABHIMANYU BANSAL <bansalabhimanyu11@gmail.com>
2020-11-12 19:10:42 +0530

    commiting the changes in files

---
+++ app.js
@@ -0,0 +1,87 @@
+new Vue({
+    el:'#vue-app',
+    data:{
+        // name:'Shaun',
+        // job:'ninja',
+        // website:'http://www.thenetninja.co.uk',
+        // websiteTag:'<a href="http://www.thenetninja.co.uk">The ninja website</a>',
+        // age:24,
+        // age1:24,
+        // X:0,
+        // Y:0,
+        // name1:'',
+        // age2:20,
+        // a:0,
+        // b:0,
+        // available:true,
+        // nearby:false,
+        // error:false,
+        // success:false,
+        // characters:['dante','vergil','leonardo','raphael','michaelanglo','sparda'],
+        // ninjas:[
+        //     {name:'donnatello',age:34},
+        //     {name:'doppleganger',age:32},
+        //     {name:'arkham',age:32},
+        //     {name:'kalina',age:23},
+        //     {name:'nevan',age:24},
+        //     {name:'dante',age:23}
+        // ],
+        // health:100,
+        // ended:false
+    },
+    methods:{
+        // greet:function(time){
+        //     return 'Good '+time+' '+this.name;
+        // },
+        // add:function(inc){
+        //     this.age1+=inc;// doesn't do addition, it does concatenation; if subtraction is done only after then addition starts
+        // },
+        // subtract:function(dec){
+        //     this.age1-=dec;
+        // },
+        // updateXY:function(event){
+        //     this.X=event.offsetX;
+        //     this.Y=event.offsetY;
+        // },
+        // click:function(){
+        //     alert('You are prevented from entering this website');
+        // },
+        // logAge:function(){
+        //     console.log('you have entered your age');
+        // },
+        // addToA:function(){
+        //     console.log('addToA')
+        //     return this.a+this.age2;
+        // },
+        // addToB:function(){
+        //     console.log('addToB')
+        //     return this.b+this.age2;
+        // },
+        // punch:function(){
+        //     this.health-=10;
+        //     if(this.health <=0){
+        //         this.ended=true;
+        //     }
+        // },
+        // restart:function(){
+        //     this.health=100;
+        //     this.ended=false;
+        // }
+    },
+    computed:{
+        // addToA:function(){
+        //     console.log('addToA')
+        //     return this.a+this.age2;
+        // },
+        // addToB:function(){
+        //     console.log('addToB')
+        //     return this.b+this.age2;
+        // },
+        // compClasses:function(){
+        //     return {
+        //         available:this.available,
+        //         nearby:this.nearby
+        //     }
+        // }
+    }
+});
